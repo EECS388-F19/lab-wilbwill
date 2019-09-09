37a4c8241e7fd10a91ac802714eedc5fd73c9403
wilbwill <38268834+wilbwill@users.noreply.github.com>
2019-09-09 12:35:30 -0400

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+import random
+
+def main():
+    print("William Wilbur")
+    x = random.randint(1,101)
+    y = random.randint(1, 101)
+    print(x)
+    print(y)
+    print("Sum = ", x + y)
+    print("Average = ", (x + y) / 2)
+
+main()
